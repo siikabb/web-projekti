f24b991ae41ddb512838a34ead4eb4dbe6d13b9a
Joona Loukkola <111729194+siikabb@users.noreply.github.com>
2023-12-03 13:06:24 +0200

    update database with cart

--- db/elysium.sql
+++ db/elysium.sql
@@ -22,17 +22,24 @@ CREATE TABLE Orders (
     order_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
     order_time TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
     user_id INT NOT NULL,
-    product_id INT NOT NULL,
     order_status INT NOT NULL DEFAULT 0,
     FOREIGN KEY (user_id) REFERENCES Users(user_id),
     FOREIGN KEY (product_id) REFERENCES Products(product_id)
 );
 
-INSERT INTO Users (email, password, user_level) VALUES ('test@metropolia.fi', 'test', 1);
-INSERT INTO Users (email, password, user_level) VALUES ('anothertest@gmail.com', 'test', 0);
-INSERT INTO Users (email, password, user_level) VALUES ('abc@aa.de', 'test', 0);
-INSERT INTO Products (name, price, description, image_url) VALUES ('Steak', 10.00, 'This is a test steak', 'https://via.placeholder.com/150');
-INSERT INTO Products (name, price, description, image_url) VALUES ('Chicken', 5.00, 'This is a test chicken', 'https://via.placeholder.com/150');
-INSERT INTO Products (name, price, description, image_url) VALUES ('Soup', 3.00, 'This is a test soup', 'https://via.placeholder.com/150');
-INSERT INTO Orders (user_id, product_id, order_status) VALUES (1, 1, 1);
-INSERT INTO Orders (user_id, product_id, order_status) VALUES (1, 2, 0);
+CREATE TABLE Cart (
+    cart_id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    order_id INT NOT NULL,
+    product_id INT NOT NULL,
+    FOREIGN KEY (order_id) REFERENCES Orders(order_id),
+    FOREIGN KEY (product_id) REFERENCES Products(product_id)
+);
+
+-- INSERT INTO Users (email, password, user_level) VALUES ('test@metropolia.fi', 'test', 1);
+-- INSERT INTO Users (email, password, user_level) VALUES ('anothertest@gmail.com', 'test', 0);
+-- INSERT INTO Users (email, password, user_level) VALUES ('abc@aa.de', 'test', 0);
+-- INSERT INTO Products (name, price, description, image_url) VALUES ('Steak', 10.00, 'This is a test steak', 'https://via.placeholder.com/150');
+-- INSERT INTO Products (name, price, description, image_url) VALUES ('Chicken', 5.00, 'This is a test chicken', 'https://via.placeholder.com/150');
+-- INSERT INTO Products (name, price, description, image_url) VALUES ('Soup', 3.00, 'This is a test soup', 'https://via.placeholder.com/150');
+-- INSERT INTO Orders (user_id, product_id, order_status) VALUES (1, 1, 1);
+-- INSERT INTO Orders (user_id, product_id, order_status) VALUES (1, 2, 0);
