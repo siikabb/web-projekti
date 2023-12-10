1774f47b456a8684c73168253e4cd12177a98153
sohaibebrahimi <ebrahimi_sa@icloud.com>
2023-12-10 20:48:52 +0200

    Update: login design finished

--- loginModal/login.js
+++ loginModal/login.js
@@ -1,25 +1,31 @@
+'use strict'
 // Get the modal
-var modal = document.getElementById("myModal");
-
+const modal = document.getElementById("loginModal");
 // Get the button that opens the modal
-var btn = document.getElementById("open-modal-btn");
-
+const openModalBtn = document.getElementById("open-modal-btn");
 // Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
+const cancelBtn = document.getElementsByClassName("close")[0];
 // When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
+openModalBtn.onclick = function() {
+  modal.style.display = "flex";
+};
 // When the user clicks on <span> (x), close the modal
-span.onclick = function() {
+cancelBtn.onclick = function() {
   modal.style.display = "none";
-}
-
+};
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
-}
+};
+// validate user input
+function loginForm() {
+  // Get form elements by their IDs
+  const usernameInput = document.getElementById("userName");
+  const passwordInput = document.getElementById("userPassword");
+
+  // Get values from the form elements
+  const usernameValue = usernameInput.value;
+  const passwordValue = passwordInput.value;
+};
