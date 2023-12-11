f42ddc390ba7454734eea43c44834e6697c72750
Joona Loukkola <111729194+siikabb@users.noreply.github.com>
2023-12-11 15:59:17 +0200

    fix server crashing on invalid login

--- src/controllers/auth-controller.mjs
+++ src/controllers/auth-controller.mjs
@@ -10,15 +10,15 @@ const postLogin = async (req, res) => {
   if (!user) {
     const error = new Error('email/password invalid');
     res.sendStatus(401);
-  }
-  // console.log('postLogin', user);
-  const match = await bcrypt.compare(req.body.password, user.password);
-  if (match) {
-    delete user.password;
-    const token = jwt.sign(user, process.env.JWT_SECRET, {expiresIn: '24h'});
-    res.json({message: 'logged in', token, user});
   } else {
-    res.sendStatus(401);
+    const match = await bcrypt.compare(req.body.password, user.password);
+    if (match) {
+      delete user.password;
+      const token = jwt.sign(user, process.env.JWT_SECRET, {expiresIn: '24h'});
+      res.json({message: 'logged in', token, user});
+    } else {
+      res.sendStatus(401);
+    }
   }
   // if (user) {
   //   res.json({...user, token});
