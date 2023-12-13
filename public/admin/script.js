9c346118f55e6d2f89b98803eabb538ef8ec6de3
Joona Loukkola <111729194+siikabb@users.noreply.github.com>
2023-12-13 13:55:56 +0200

    add deleting items to admin page

--- public/admin/script.js
+++ public/admin/script.js
@@ -75,16 +75,39 @@ function resetForm() {
 }
 
 // Function to handle deleting a menu item
-function deleteMenuItem(itemId) {
+async function deleteMenuItem(itemId) {
   if (confirm('Are you sure you want to delete this menu item?')) {
-    const menuTable = document.querySelector('.menu-items tbody');
-    const rows = menuTable.querySelectorAll('tr');
-    for (const row of rows) {
-      if (row.firstChild.textContent === itemId) {
-        row.remove();
-        break;
-      }
-    }
+    // const menuTable = document.querySelector('.menu-items tbody');
+    // const rows = menuTable.querySelectorAll('tr');
+    // for (const row of rows) {
+    //   if (row.firstChild.textContent === itemId) {
+    //     row.remove();
+    //     break;
+    //   }
+    // }
+    console.log(itemId);
+    await fetch(`../products/${itemId}/`, {
+      method: 'DELETE',
+      headers: {
+        Accept: 'application/json, text/plain, */*',
+        'Content-Type': 'application/json',
+        Authorization: 'Bearer ' + token,
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data);
+      });
+    //   if (data.user.user_level === 0) {
+    //     console.log('success');
+    //     document.querySelector('.content').style.display = 'block';
+    //     document.querySelector('.login').style.display = 'none';
+    //     document.querySelector('.sidebar').style.display = 'block';
+    //     token = data.token;
+    //     console.log(token);
+    //   }
+    // });
+    await updateMenuItems();
   }
 }
 
@@ -111,7 +134,9 @@ document
   .addEventListener('click', function (event) {
     const target = event.target;
     const row = target.closest('tr');
-    const itemId = row.firstChild.textContent;
+    const itemId = row.querySelector('.product_id').textContent;
+    console.log(row);
+    console.log(itemId);
 
     if (target.classList.contains('edit-btn')) {
       // Set form values to selected row's values and switch to edit mode
@@ -201,7 +226,7 @@ login.addEventListener('submit', (event) => {
     });
 });
 
-const updateMenuItems = () => {
+const updateMenuItems = async () => {
   const menuTable = document.querySelector('.menu-items tbody');
   menuTable.innerHTML = '';
   const url = '../products/';
@@ -213,7 +238,7 @@ const updateMenuItems = () => {
         console.log(product);
         const newRow = document.createElement('tr');
         newRow.innerHTML = `
-                <td>${product.product_id}</td>
+                <td class="product_id">${product.product_id}</td>
                 <td>dishtype</td>
                 <td>${product.name}</td>
                 <td>${product.price} €</td>
