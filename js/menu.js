9274e4de3029712dd456b0512032f725de22029a
Aputiti <abdur.abouramadan@gmail.com>
2023-12-14 18:05:03 +0200

    fine tuning

--- js/menu.js
+++ js/menu.js
@@ -1,3 +1,5 @@
+'use strict';
+
 const openButton = document.querySelector('.rm-button-open');
 const container = document.querySelector('#rm-container');
 const closeButton = document.querySelector('.rm-button-open2');
@@ -85,12 +87,11 @@ if (isFirefox) {
   // Code for Safari
 } else {
   // Code for other browsers (Chrome)
-  mouseOverContainer.onmousemove = (e) => {
-    let xy = [e.clientX, e.clientY];
-    let position = xy.concat([ex1Layer]);
-
-    window.requestAnimationFrame(() => {
-      transformElement(ex1Layer, position);
-    });
-  };
+  // mouseOverContainer.onmousemove = (e) => {
+  //   let xy = [e.clientX, e.clientY];
+  //   let position = xy.concat([ex1Layer]);
+  //   window.requestAnimationFrame(() => {
+  //     transformElement(ex1Layer, position);
+  //   });
+  // };
 }
