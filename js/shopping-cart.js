5184ddfdb07fe6b5dec0405e03374adef15d9e16
Aputiti <abdur.abouramadan@gmail.com>
2023-12-14 16:20:10 +0200

    added notifications

--- js/shopping-cart.js
+++ js/shopping-cart.js
@@ -3,6 +3,7 @@
 const shoppingCart = document.querySelector('.shopping-cart');
 const addButtons = document.querySelectorAll('.add-btn');
 const orderButton = document.querySelector('.order-button');
+const deleteButtons = document.querySelectorAll('.delete-item');
 
 // Adding an item to the shopping cart
 const addItem = (itemName, itemPrice) => {
@@ -17,10 +18,39 @@ const addItem = (itemName, itemPrice) => {
   itemPriceElement.classList.add('item-price');
   itemPriceElement.textContent = '$' + itemPrice.toFixed(2);
 
+  const itemDeleteElement = document.createElement('li');
+  itemDeleteElement.classList.add('delete-item');
+  itemDeleteElement.textContent = '-';
+
+  // Delete button
+  itemDeleteElement.addEventListener('click', () => {
+    newItem.remove();
+  });
+
   newItem.appendChild(itemNameElement);
   newItem.appendChild(itemPriceElement);
+  newItem.appendChild(itemDeleteElement);
 
   shoppingCart.appendChild(newItem);
+};
+
+// Notification
+const showNotification = () => {
+  const notification = document.getElementById('notification');
+
+  notification.classList.add('show');
+  notification.classList.remove('hidden');
+
+  setTimeout(function () {
+    hideNotification();
+  }, 1000);
+};
+
+const hideNotification = () => {
+  const notification = document.getElementById('notification');
+
+  notification.classList.add('hidden');
+  notification.classList.remove('show');
 };
 
 // Check if addButton is clicked
@@ -29,12 +59,13 @@ addButtons.forEach((button) => {
     const itemName = button.parentElement.firstChild.textContent
       .trim()
       .replace(/-/g, '');
-    console.log(itemName);
+    // console.log(itemName);
     const itemPrice = parseFloat(
       button.parentElement.querySelector('.price').textContent.replace('$', '')
     );
-    console.log(itemPrice);
+    // console.log(itemPrice);
 
     addItem(itemName, itemPrice);
+    showNotification();
   });
 });
