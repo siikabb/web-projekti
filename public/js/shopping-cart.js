b486bb88de4c6678c9696678b5a0a978008af6ca
Joona Loukkola <111729194+siikabb@users.noreply.github.com>
2023-12-14 16:32:20 +0200

    refactor

---
+++ public/js/shopping-cart.js
@@ -0,0 +1,71 @@
+'use strict';
+
+const shoppingCart = document.querySelector('.shopping-cart');
+const addButtons = document.querySelectorAll('.add-btn');
+const orderButton = document.querySelector('.order-button');
+const deleteButtons = document.querySelectorAll('.delete-item');
+
+// Adding an item to the shopping cart
+const addItem = (itemName, itemPrice) => {
+  const newItem = document.createElement('ul');
+  newItem.classList.add('item');
+
+  const itemNameElement = document.createElement('li');
+  itemNameElement.classList.add('item-name');
+  itemNameElement.textContent = itemName;
+
+  const itemPriceElement = document.createElement('li');
+  itemPriceElement.classList.add('item-price');
+  itemPriceElement.textContent = '$' + itemPrice.toFixed(2);
+
+  const itemDeleteElement = document.createElement('li');
+  itemDeleteElement.classList.add('delete-item');
+  itemDeleteElement.textContent = '-';
+
+  // Delete button
+  itemDeleteElement.addEventListener('click', () => {
+    newItem.remove();
+  });
+
+  newItem.appendChild(itemNameElement);
+  newItem.appendChild(itemPriceElement);
+  newItem.appendChild(itemDeleteElement);
+
+  shoppingCart.appendChild(newItem);
+};
+
+// Notification
+const showNotification = () => {
+  const notification = document.getElementById('notification');
+
+  notification.classList.add('show');
+  notification.classList.remove('hidden');
+
+  setTimeout(function () {
+    hideNotification();
+  }, 1000);
+};
+
+const hideNotification = () => {
+  const notification = document.getElementById('notification');
+
+  notification.classList.add('hidden');
+  notification.classList.remove('show');
+};
+
+// Check if addButton is clicked
+addButtons.forEach((button) => {
+  button.addEventListener('click', () => {
+    const itemName = button.parentElement.firstChild.textContent
+      .trim()
+      .replace(/-/g, '');
+    // console.log(itemName);
+    const itemPrice = parseFloat(
+      button.parentElement.querySelector('.price').textContent.replace('$', '')
+    );
+    // console.log(itemPrice);
+
+    addItem(itemName, itemPrice);
+    showNotification();
+  });
+});
